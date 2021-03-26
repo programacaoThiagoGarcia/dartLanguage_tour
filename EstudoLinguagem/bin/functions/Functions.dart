292c7787cf12c2119d40b8861596f074f5a49dd8
ThiagoGarcia <thiago.garcia@itsector.pt>
2021-03-26 17:32:09 +0000

    first commit

---
+++ EstudoLinguagem/bin/functions/Functions.dart
@@ -0,0 +1,86 @@
+
+/*
+  Functions in Dart
+  https://dart.dev/guides/language/language-tour#functions
+ */
+
+void main(List<String> arguments) {
+  var res = isEqualFunction(5, 6);
+  print(res);
+  functionNamedParameters(msg: 'Hello');
+  functionNamedParameters();
+  functionWithParamtersRequire(name: 'olá');
+  print(sayHello('Thiago', 'Hello'));
+  print(sayHello('Thiago', 'Hello','iphone'));
+
+  functionAsParameters((msg) {
+    var _msg = msg.toUpperCase();
+    print(_msg);
+  });
+
+  functionAsParameters((msg) => print(msg));
+
+
+  var complete = completeName('thiago');
+  print(complete('Garcia'));
+
+
+  usingClosure(index: 10, completion: (name){
+    var _name = completeName('Thiago');
+    print(_name(name));
+  });
+
+
+  usingClosure(index: 5, completion: completion);
+
+}
+
+bool isEqualFunction(num1, num2) => num1 == num2;
+void functionNamedParameters({String?  msg}){
+  if(msg != null){
+    print(msg);
+    return;
+  }
+  print('nulo');
+}
+void functionWithParamtersRequire({required String name}) => print('Name: '+name);
+
+String sayHello(String from, String msg, [String? device]){
+  var result = '$from says $msg';
+  if (device != null) {
+    result = '$result with a $device';
+  }
+  return result;
+}
+
+
+//=============================================================================
+//           Function as a parameters
+
+
+void functionAsParameters(void Function(String msg) completion){
+  for(var i = 0; i < 10; i++){
+    print(i);
+  }
+  completion('Hello completion');
+}
+
+
+//============================================================================
+// Closures
+
+Function completeName(String name){
+  return (String lastName) => name +' '+lastName;
+}
+
+// ignore: use_function_type_syntax_for_parameters
+void usingClosure({required int index, required void Function (String name) completion}){
+  for(var i = 0; i < index; i++){
+    print(i);
+  }
+  completion('Gomes');
+}
+
+void completion(String msg ){
+  print(msg);
+}
